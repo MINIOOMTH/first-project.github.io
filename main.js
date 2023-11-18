9534aa85bc3ef06f2b050fdf7a33e1b0069fd18b
MINIOOMTH <52069703+MINIOOMTH@users.noreply.github.com>
2023-11-18 18:26:55 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,51 @@ function OnSearch() {
         }
     }
 }
+
+// นำ URL ของไฟล์ .txt มาใส่ที่นี่
+const txtFileUrl = 'thetext.txt';
+// ใช้ fetch เพื่อโหลดข้อมูลจากไฟล์ .txt
+fetch(txtFileUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.text();
+  })
+  .then(textData => {
+    // แสดงข้อมูลใน div ที่มี id เท่ากับ "content"
+    document.getElementById('content').textContent = textData;
+  })
+  .catch(error => {
+    console.error('Error fetching the text file:', error);
+  });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const messageElement = document.getElementById("message");
+    const InSpan = document.getElementById("InSpan");
+
+    function updateMessage() {
+        const now = new Date();
+        const dayOfWeek = now.getDay(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+
+        const isWeekdayAndDayTime = dayOfWeek >= 1 && dayOfWeek <= 5 && hours >= 7 && minutes >= 0 && hours <= 20 && minutes <= 0;
+
+        if (isWeekdayAndDayTime) {
+            messageElement.textContent = "เปิดบริการ";
+            InSpan.className = "text-success spinner-border spinner-border-sm";
+            messageElement.className = "text-success"; // ใช้สีเขียว
+        } else {
+            messageElement.textContent = "ปิดบริการ";
+            InSpan.className = "text-danger spinner-border spinner-border-sm";
+            messageElement.className = "text-danger"; // ใช้สีแดง
+        }
+    }
+
+    // ประมวลผลเพื่อให้แสดงข้อความและสีที่ถูกต้องทุกๆ 1 นาที
+    updateMessage();
+    setInterval(updateMessage, 60000);
+});
+  
